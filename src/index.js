447d98ec80b897b7bab5cfd868fe04f8b1786a64
murhuh <murhuh@inbox.ru>
2022-09-27 15:29:02 +0300

    обязательное задание из урока 21

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import menu from './modules/menu';
 
 import modal from './modules/modal';
 
+import validate from './modules/validation';
+
 timer('22 september 2022');
 
 menu();
 
 modal();
+
+validate();
---
+++ src/modules/validation.js
@@ -0,0 +1,40 @@
+
+const validate = () => {
+
+  const calcItem = document.querySelectorAll('.calc-item'),
+        userName = document.querySelectorAll('[name="user_name"]'),
+        userEmail = document.querySelectorAll('[name="email_name"]'),
+        userPhone = document.querySelectorAll('[name="user_phone"]');
+
+  const cyrillicValidation = (e) => {
+    e.target.value = e.target.value.replace(/[^а-яА-я\-\s]/, '');
+  };
+
+  const emailValidation = (e) => {
+    e.target.value = e.target.value.replace(/\S+[\w\.-]+@\S+\.\S+/, '');
+  };
+
+  const phoneValidation = (e) => {
+    e.target.value = e.target.value.replace(/[a-zА-я!@#$%&*_=]+$/, '');
+  };
+
+  calcItem.forEach((item) => {
+    item.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^0-9\.]/, '');
+    });
+  });
+
+  userName.forEach((item) => {
+    item.addEventListener('input', cyrillicValidation);
+  });
+
+  userEmail.forEach((item) => {
+    item.addEventListener('input', emailValidation);
+  });
+
+  userPhone.forEach((item) => {
+    item.addEventListener('input', phoneValidation);
+  });
+};
+
+export default validate;
